a7538b8166282ae006b870a5cbdf8dbfca818e8a
cyri113 <c.derche@me.com>
2025-02-06 14:41:52 +0100

    destroy ui app

--- ui/src/config/index.ts
+++
@@ -1,1 +0,0 @@
-export { wagmiAdapter } from './wagmi.adapter'
--- ui/src/contracts/predictionV2/contract.ts
+++
@@ -1,7 +0,0 @@
-import { base, baseSepolia } from "viem/chains";
-export { abi } from './abi.json';
-
-export const address = {
-  [base.id]: '',
-  [baseSepolia.id]: ''
-}
