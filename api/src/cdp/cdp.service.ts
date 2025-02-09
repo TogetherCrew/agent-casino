9eb2d14437820c46f06f714c8c8cf8da87ea2c3b
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2025-02-09 13:41:16 +0200

    feat: add get, create and transfer methods to cdp

--- api/src/cdp/cdp.service.ts
+++ api/src/cdp/cdp.service.ts
@@ -1,34 +1,58 @@
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino'
 
-import { Coinbase, Wallet } from '@coinbase/coinbase-sdk'
+import { Coinbase, Destination, Wallet } from '@coinbase/coinbase-sdk'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class CdpService {
+    private readonly apiKeyName: string
+    private readonly privateKey: string
+
     constructor(
         private readonly configService: ConfigService,
         @InjectPinoLogger(CdpService.name)
         private readonly logger: PinoLogger
-    ) {}
+    ) {
+        this.apiKeyName = this.configService.get<string>('cdp.apiKeyName')
+        this.privateKey = this.configService.get<string>('cdp.privateKey')
+        Coinbase.configure({
+            apiKeyName: this.apiKeyName,
+            privateKey: this.privateKey,
+        })
+        Coinbase.useServerSigner = true
+    }
 
-    public async createMpcWallet() {
+    public async createWallet() {
         try {
-            const apiKeyName = this.configService.get<string>('cdp.apiKeyName')
-            const privateKey = this.configService.get<string>('cdp.privateKey')
-
-            Coinbase.configure({
-                apiKeyName,
-                privateKey,
+            return await Wallet.create({
+                networkId: Coinbase.networks.BaseSepolia,
             })
-
-            const wallet = await Wallet.create()
-            this.logger.info({ wallet }, 'MPC wallet created')
-
-            return wallet
         } catch (err) {
             this.logger.error({ err }, 'Failed to create MPC Wallet')
             throw err
         }
+    }
+
+    public async getWallet(walletId: string) {
+        try {
+            return Wallet.fetch(walletId)
+        } catch (err) {
+            this.logger.error({ err }, 'Failed to create MPC Wallet')
+            throw err
+        }
+    }
+
+    public async createTransfer(wallet: Wallet, destination: Destination) {
+        console.log(destination, typeof destination)
+
+        const transfer = await wallet.createTransfer({
+            amount: 0.000001,
+            assetId: Coinbase.assets.Eth,
+            destination,
+        })
+
+        await transfer.wait()
+        return transfer
     }
 }
