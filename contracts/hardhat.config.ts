c2f23ebdff9eebebd3f7d72f1a40662e0dcd5705
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2025-02-09 17:30:02 +0200

    chore: add base hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,38 +1,43 @@
-import '@nomicfoundation/hardhat-toolbox-viem'
-import '@nomiclabs/hardhat-solhint'
+import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomiclabs/hardhat-solhint";
 
-import { HardhatUserConfig, vars } from 'hardhat/config'
-import { generatePrivateKey } from 'viem/accounts'
+import { HardhatUserConfig, vars } from "hardhat/config";
+import { generatePrivateKey } from "viem/accounts";
 
-const PRIVATE_KEY = vars.has('PRIVATE_KEY')
-    ? vars.get('PRIVATE_KEY')
-    : generatePrivateKey()
-const BASESCAN_API_KEY = vars.has('BASESCAN_API_KEY')
-    ? vars.get('BASESCAN_API_KEY')
-    : ''
+const PRIVATE_KEY = vars.has("PRIVATE_KEY")
+  ? vars.get("PRIVATE_KEY")
+  : generatePrivateKey();
+const BASESCAN_API_KEY = vars.has("BASESCAN_API_KEY")
+  ? vars.get("BASESCAN_API_KEY")
+  : "";
 
 const config: HardhatUserConfig = {
-    solidity: {
-        version: '0.8.26',
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 1000,
-            },
-        },
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
     },
-    networks: {
-        baseSepolia: {
-            chainId: 84532,
-            accounts: [PRIVATE_KEY],
-            url: 'https://sepolia.base.org',
-        },
+  },
+  networks: {
+    base: {
+      chainId: 8453,
+      accounts: [PRIVATE_KEY],
+      url: "https://mainnet.base.org",
     },
-    etherscan: {
-        apiKey: {
-            baseSepolia: BASESCAN_API_KEY,
-        },
+    baseSepolia: {
+      chainId: 84532,
+      accounts: [PRIVATE_KEY],
+      url: "https://sepolia.base.org",
     },
-}
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: BASESCAN_API_KEY,
+    },
+  },
+};
 
-export default config
+export default config;
